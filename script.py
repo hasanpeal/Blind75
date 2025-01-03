fe904fbbc92b7ec9241dd7cc5498f2aa47d13334
Peal Hasan <pealh0320@gmail.com>
2025-01-03 18:02:32 -0500

    Daily Practice

--- script.py
+++ script.py
@@ -1,20 +1,7 @@
 import os
 import subprocess
 
-def count_files_in_directory(directory):
-    """Count the total number of files in the directory."""
-    total_files = sum(len(files) for _, _, files in os.walk(directory))
-    return total_files - 2
-
 def git_automation():
-    """Automate git commands and print file count."""
-    # Get the current directory
-    current_directory = os.getcwd()
-
-    # Count total files in the directory
-    total_files = count_files_in_directory(current_directory)
-    print(f"Total files in the directory - 2: {total_files}")
-
     # Execute git commands
     try:
         subprocess.run(["git", "add", "."], check=True)
