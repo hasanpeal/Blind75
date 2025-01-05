18cdf912d862b35c2603bcf6e320d20bc51f15db
Peal Hasan <pealh0320@gmail.com>
2025-01-05 12:40:36 -0500

    Daily Practice

--- script.py
+++ script.py
@@ -7,8 +7,8 @@ def git_automation():
         subprocess.run(["git", "commit", "-m", "Daily Practice"], check=True)
         subprocess.run(["git", "push"], check=True)
         print("Git commands executed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred while running git commands: {e}")
+    except:
+        print(f"An error occurred while running git commands")
 
 if __name__ == "__main__":
     git_automation()
