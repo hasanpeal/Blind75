d4299b7331d55100ad13477eb749396483429f60
Peal Hasan <pealh0320@gmail.com>
2025-01-17 14:58:27 -0500

    Daily practice: 2025-01-17 14:58:27

--- practice.py
+++ practice.py
@@ -1,11 +1,2 @@
-def characterReplacement(self, s: str, k: int) -> int:
-        seen = {}
-        l,r,m = 0, 0, 0
-        while r < len(s):
-            seen[s[r]] = 1 + seen.get(s[r], 0)
-            while ((r - l + 1) - max(seen.values())) > k:
-                seen[s[l]] -= 1
-                l += 1
-            m = max(m, r - l + 1)
-            r += 1
-        return m
+gitscript.py
+script.py
