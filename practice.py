6303d12602619cb5ab980037d68158d4ddb4eb92
Peal Hasan <pealh0320@gmail.com>
2025-01-18 12:08:12 -0500

    Daily practice: 2025-01-18 12:08:12

--- practice.py
+++ practice.py
@@ -1,11 +1,23 @@
-from typing import List
+from typing import Optional
 
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        has = {}
-        for i, v in enumerate(numbers):
-            has[v] = i
-        for i in range(len(numbers)):
-            key = target - numbers[i]
-            if key in has and has.get(key) != i:
-                return [min(i+1, has.get(key) + 1), max(i+1, has.get(key) + 1)]
-        
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot or self.isSameTree(root, subRoot):
+            return True
+        if not root:
+            return False
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+         
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        elif (p and q) and (p.val == q.val):
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
