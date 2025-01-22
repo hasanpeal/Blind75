ad84a8cc6cd2baf2fbec1623d00ed447548203ed
Peal Hasan <pealh0320@gmail.com>
2025-01-22 11:22:25 -0500

    Daily practice: 2025-01-22 11:22:25

--- practice.py
+++ practice.py
@@ -1,14 +1,2 @@
-from typing import List
-
-def maxArea(self, heights: List[int]) -> int:
-        left, right = 0, len(heights) - 1
-        maximum = 0
-
-        while (left < right):
-            currMax = (right - left) * min(heights[left], heights[right])
-            maximum = max(currMax, maximum)
-            if(heights[left] <= heights[right]):
-                left += 1
-            else:
-                right -= 1
-        return maximum
+gitscript.py
+script.py
