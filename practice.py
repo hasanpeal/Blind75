ee6a1acf038a6c75f43b56eac30428b1d44f2e91
Peal Hasan <pealh0320@gmail.com>
2025-01-25 19:35:33 -0500

    Daily practice: 2025-01-25 19:35:33

--- practice.py
+++ practice.py
@@ -1,28 +1,23 @@
-from typing import List, Optional
+from typing import Optional
 
-class ListNode:
-    def __init__(self, val=0, next=None):
+class Node:
+    def __init__(self, val = 0, neighbors = None):
         self.val = val
-        self.next = next
+        self.neighbors = neighbors if neighbors is not None else []
 
-class Solution:    
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
             return None
-        for i in range(1, len(lists)):
-            lists[i] = self.merge(lists[i-1], lists[i])
-        return lists[-1]
+            
+        oldToNew = {}
 
-    def merge(self, list1, list2):
-        dummy = ListNode()
-        curr = dummy
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next
-        curr.next = list1 or list2
-        return dummy.next
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        return dfs(node)
