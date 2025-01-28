4b7636ada54a0b9185b29ca937f26b24e9e0dcc2
Peal Hasan <pealh0320@gmail.com>
2025-01-27 22:45:44 -0500

    Daily practice: 2025-01-27 22:45:44

--- practice.py
+++ practice.py
@@ -1,19 +1,28 @@
-from typing import List, Optional
+import heapq
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+class MedianFinder:
 
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        # Root is always in preorder[0]
-        root = TreeNode(preorder[0])
-        # Finding root in inorder array
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
+    def __init__(self):
+        # Store approximate equal values in 2 heap for accessing max and min in O(1)
+        self.maxHeap, self.minHeap =[], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxHeap, -1 * num)
+        # Make sure values in heap are there then if highest val of maxHeap greater than minHeaps min then swap
+        if (self.maxHeap and self.minHeap) and -1 * self.maxHeap[0] > self.minHeap[0]:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+        # The length of both heaps difference can't be greater than 1 if it's then swap to equalize
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            val = 1 * heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.maxHeap) > len(self.minHeap):
+            return -1 * self.maxHeap[0]
+        if len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        return ((-1 * self.maxHeap[0]) + self.minHeap[0])/2
