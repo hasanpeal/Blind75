e59ed23fd015cf8325cb6326b9fbebdb20094c1a
Peal Hasan <pealh0320@gmail.com>
2025-01-31 18:37:07 -0500

    Daily practice: 2025-01-31 18:37:07

--- practice.py
+++ practice.py
@@ -1,20 +1,12 @@
 from typing import List
 
-def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        res = 1
-        has = set()
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
         for num in nums:
-            has.add(num)
-        for i in range(len(nums)):
-            curr = nums[i]
-            longest = 1
-            if curr - 1 in has:
-                continue
-            while curr + 1 in has:
-                curr += 1
-                longest += 1
-            res = max(res, longest)
-        return res
-            
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+        return False
