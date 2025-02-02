9b8a60019eee3785088bd203b71fd9a9dae6a749
Peal Hasan <pealh0320@gmail.com>
2025-02-02 16:26:08 -0500

    Daily practice: 2025-02-02 16:26:08

--- practice.py
+++ practice.py
@@ -1,14 +1,30 @@
 from typing import Optional
+from LinkedListCycle import ListNode
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+def __init__(self, val=0, next=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.next = next
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return max((1 + self.maxDepth(root.left)), ((1 + self.maxDepth(root.right))))
-        
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        curr = slow.next
+        slow.next = None
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow = head
+        fast = prev
+        while fast:
+            temp1 = slow.next
+            temp2 = fast.next
+            slow.next = fast
+            fast.next = temp1
+            slow = temp1
+            fast = temp2
