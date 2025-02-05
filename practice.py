6c7f7a934eb9f85695d3a3e4f34e77bcbe9fb746
Peal Hasan <pealh0320@gmail.com>
2025-02-05 10:54:21 -0500

    Daily practice: 2025-02-05 10:54:21

--- practice.py
+++ practice.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -5,14 +7,8 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return None
-        curr = root
-        while curr:
-            if p.val < curr.val and q.val < curr.val:
-                curr = curr.left
-            elif p.val > curr.val and q.val > curr.val:
-                curr = curr.right
-            else:
-                return curr
+            return 0
+        return max((1 + self.maxDepth(root.left)), ((1 + self.maxDepth(root.right))))
+        
