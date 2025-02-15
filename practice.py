6c43043e7fed321f4cf0a168ff64694afdd235b6
Peal Hasan <pealh0320@gmail.com>
2025-02-15 10:16:28 -0500

    Daily practice: 2025-02-15 10:16:28

--- practice.py
+++ practice.py
@@ -1,16 +1,2 @@
-from typing import List
-
-def findMin(self, nums: List[int]) -> int:
-        l, r, res = 0, len(nums) - 1, nums[0]
-        # It's '<=' because what if we have [2] here l val = 2 r val = 2, it would skip loop for '<'
-        while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
-                break
-            m = (l + r) // 2
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
-            else:
-                r = m - 1
-        return res
+gitscript.py
+script.py
