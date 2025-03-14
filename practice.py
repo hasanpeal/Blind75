119af17b05e3f611dcedcfa232ecb59c7663650a
Peal Hasan <pealh0320@gmail.com>
2025-03-14 15:07:03 -0400

    Daily practice: 2025-03-14 15:07:03

--- practice.py
+++ practice.py
@@ -1,14 +1,12 @@
-from typing import List
-
-def maxArea(self, heights: List[int]) -> int:
-        left, right = 0, len(heights) - 1
-        maximum = 0
-
-        while (left < right):
-            currMax = (right - left) * min(heights[left], heights[right])
-            maximum = max(currMax, maximum)
-            if(heights[left] <= heights[right]):
-                left += 1
-            else:
-                right -= 1
-        return maximum
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
+        
+        
