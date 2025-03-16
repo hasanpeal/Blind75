7050c09c4cd1eb80dc8baa25d722054dede33c00
Peal Hasan <pealh0320@gmail.com>
2025-03-15 20:09:10 -0400

    Daily practice: 2025-03-15 20:09:10

--- practice.py
+++ practice.py
@@ -1,12 +1,12 @@
-def lengthOfLongestSubstring(s: str) -> int:
-        seen = set()
-        left, right, longest = 0, 0, 0
-        while(right < len(s)):
-            if s[right] not in seen:
-                seen.add(s[right])
-                longest = max(longest, len(seen))
-                right += 1
-            else:
-                seen.remove(s[left])
-                left += 1
-        return longest
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
+        
+        
