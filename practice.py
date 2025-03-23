8d0a0ffe4cb94b0ebe1adada3d8a20a3751839e1
Peal Hasan <pealh0320@gmail.com>
2025-03-22 21:11:27 -0400

    Daily practice: 2025-03-22 21:11:27

--- practice.py
+++ practice.py
@@ -7,14 +7,12 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        nodes = []
-        # Inorder traversal sorted the BST
-        def dfs(node):
-            if not node:
-                return
-            dfs(node.left)
-            nodes.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return nodes[k - 1]
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
