a10a2a196b628f755cb66c58b5132fb937dd33b9
Peal Hasan <pealh0320@gmail.com>
2025-04-07 13:58:50 -0400

    Daily practice: 2025-04-07 13:58:50

--- practice.py
+++ practice.py
@@ -1,16 +1,13 @@
-from typing import List
-
-def findMin(self, nums: List[int]) -> int:
-        l, r, res = 0, len(nums) - 1, nums[0]
-        # It's '<=' because what if we have [2] here l val = 2 r val = 2, it would skip loop for '<'
-        while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
-                break
-            m = (l + r) // 2
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
+def isValid(self, s: str) -> bool:
+        stack = []
+        close = { ')':'(', '}':'{', ']':'['}
+        for c in s:
+            if c in close:
+                # Top of stack: stack[-1], we check first if elements exist in stack to avoid error
+                if stack and stack[-1] == close[c]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                r = m - 1
-        return res
+                stack.append(c)
+        return True if not stack else False
