3673318a3f1f49a4dff6543224df055c69dd4b02
Peal Hasan <pealh0320@gmail.com>
2025-04-09 12:46:35 -0400

    Daily practice: 2025-04-09 12:46:35

--- practice.py
+++ practice.py
@@ -1,20 +1,2 @@
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        nodes = []
-        # Inorder traversal sorted the BST
-        def dfs(node):
-            if not node:
-                return
-            dfs(node.left)
-            nodes.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return nodes[k - 1]
+gitscript.py
+script.py
