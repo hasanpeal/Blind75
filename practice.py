5a02cd21817feb0aee476aeeae1199fe0a2a36c3
Peal Hasan <pealh0320@gmail.com>
2025-04-14 14:04:17 -0400

    Daily practice: 2025-04-14 14:04:17

--- practice.py
+++ practice.py
@@ -1,38 +1,28 @@
-from collections import defaultdict
-from typing import List
+import heapq
 
-class Solution:
-    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # Step 1: Build the adjacency list to represent the graph
-        # The graph is represented as a dictionary where each node maps to a list of its neighbors
-        adj = defaultdict(list)
-        for a, b in edges:
-            # Add both directions for the undirected graph
-            adj[a].append(b)
-            adj[b].append(a)
-        
-        # Step 2: Initialize a set to keep track of visited nodes
-        visited = set()
+class MedianFinder:
 
-        # Step 3: Define a DFS function to traverse the graph
-        def dfs(node):
-            # Mark the current node as visited
-            visited.add(node)
-            # Traverse all unvisited neighbors of the current node
-            for nei in adj[node]:
-                if nei not in visited:
-                    dfs(nei)
-        
-        # Step 4: Initialize a counter for the number of connected components
-        count = 0
-        
-        # Step 5: Iterate through all nodes
-        for i in range(n):
-            # If a node is not visited, it's part of a new connected component
-            if i not in visited:
-                count += 1  # Increment the count for a new component
-                # Perform DFS to mark all nodes in this component as visited
-                dfs(i)
-        
-        # Step 6: Return the total number of connected components
-        return count
+    def __init__(self):
+        # Store approximate equal values in 2 heap for accessing max and min in O(1)
+        self.maxHeap, self.minHeap =[], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxHeap, -1 * num)
+        # Make sure values in heap are there then if highest val of maxHeap greater than minHeaps min then swap
+        if (self.maxHeap and self.minHeap) and -1 * self.maxHeap[0] > self.minHeap[0]:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+        # The length of both heaps difference can't be greater than 1 if it's then swap to equalize
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            val = 1 * heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.maxHeap) > len(self.minHeap):
+            return -1 * self.maxHeap[0]
+        if len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        return ((-1 * self.maxHeap[0]) + self.minHeap[0])/2
