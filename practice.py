d1fca692fe39ccd5c72f3c02f9a8e56aaf1afa90
Peal Hasan <pealh0320@gmail.com>
2025-04-21 21:48:44 -0400

    Daily practice: 2025-04-21 21:48:44

--- practice.py
+++ practice.py
@@ -1,20 +1,30 @@
 from typing import Optional
+from LinkedListCycle import ListNode
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+def __init__(self, val=0, next=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.next = next
 
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        nodes = []
-        # Inorder traversal sorted the BST
-        def dfs(node):
-            if not node:
-                return
-            dfs(node.left)
-            nodes.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return nodes[k - 1]
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        curr = slow.next
+        slow.next = None
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow = head
+        fast = prev
+        while fast:
+            temp1 = slow.next
+            temp2 = fast.next
+            slow.next = fast
+            fast.next = temp1
+            slow = temp1
+            fast = temp2
