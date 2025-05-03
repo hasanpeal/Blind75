44ad20d2d0167cdf257cf210b4240b3b2cc0e830
Peal Hasan <pealh0320@gmail.com>
2025-05-03 14:51:52 -0400

    Daily practice: 2025-05-03 14:51:52

--- practice.py
+++ practice.py
@@ -1,14 +1,2 @@
-from typing import Optional
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        return prev
+gitscript.py
+script.py
