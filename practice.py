94c976e207fd38deedfb7142bf719508f40d4fff
Peal Hasan <pealh0320@gmail.com>
2025-05-13 21:37:12 -0400

    Daily practice: 2025-05-13 21:37:12

--- practice.py
+++ practice.py
@@ -1,12 +1,16 @@
-from typing import List
+from typing import Optional
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            else:
-                seen.add(num)
-        return False
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif ((p and q) and (p.val == q.val)):
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
