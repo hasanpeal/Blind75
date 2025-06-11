3cff6febcbb3b13cf15f676052713d3749d42e6c
Peal Hasan <pealh0320@gmail.com>
2025-06-11 16:24:55 -0500

    Daily practice: 2025-06-11 16:24:55

--- practice.py
+++ practice.py
@@ -1,16 +1,12 @@
-from typing import List
-
-def findMin(self, nums: List[int]) -> int:
-        l, r, res = 0, len(nums) - 1, nums[0]
-        # It's '<=' because what if we have [2] here l val = 2 r val = 2, it would skip loop for '<'
-        while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
-                break
-            m = (l + r) // 2
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
+def lengthOfLongestSubstring(s: str) -> int:
+        seen = set()
+        left, right, longest = 0, 0, 0
+        while(right < len(s)):
+            if s[right] not in seen:
+                seen.add(s[right])
+                longest = max(longest, len(seen))
+                right += 1
             else:
-                r = m - 1
-        return res
+                seen.remove(s[left])
+                left += 1
+        return longest
