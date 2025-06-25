3fc2b2d33c63827604fb17772ab0ed2fa93298bc
Peal Hasan <pealh0320@gmail.com>
2025-06-25 18:09:23 -0500

    Daily practice: 2025-06-25 18:09:23

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -7,14 +7,16 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        nodes = []
-        # Inorder traversal sorted the BST
-        def dfs(node):
-            if not node:
-                return
-            dfs(node.left)
-            nodes.append(node.val)
-            dfs(node.right)
-        dfs(root)
-        return nodes[k - 1]
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            
+            
+            
+            return None
+        # Root is always in preorder[0]
+        root = TreeNode(preorder[0])
+        # Finding root in inorder array
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
