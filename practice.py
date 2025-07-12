1c882a4be6a9915073fa1ff41b17d1bdd24dd582
Peal Hasan <pealh0320@gmail.com>
2025-07-11 21:44:08 -0500

    Daily practice: 2025-07-11 21:44:08

--- practice.py
+++ practice.py
@@ -1,25 +1,2 @@
-from typing import List
-
-def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preq = {i : [] for i in range(numCourses)}
-        for c, p in prerequisites:
-            preq[c].append(p)
-        visited = set()
-
-        def dfsCycleDetect(c):
-            if c in visited:
-                return False
-            if preq[c] == []:
-                return True
-            visited.add(c)
-            for pre in preq[c]:
-                if not dfsCycleDetect(pre):
-                    return False
-            visited.remove(c)
-            preq[c] = []
-            return True
-        
-        for p in range(numCourses):
-            if not dfsCycleDetect(p):
-                return False
-        return True
+gitscript.py
+script.py
