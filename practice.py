e90c272d3b62807e0aa3fb4fc188c339a1c025dc
Peal Hasan <pealh0320@gmail.com>
2025-07-18 15:37:19 -0500

    Daily practice: 2025-07-18 15:37:19

--- practice.py
+++ practice.py
@@ -1,30 +1,12 @@
-from typing import Optional
-from LinkedListCycle import ListNode
-
-def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
 class Solution:
-    def reorderList(self, head: Optional[ListNode]) -> None:
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        curr = slow.next
-        slow.next = None
-        prev = None
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        slow = head
-        fast = prev
-        while fast:
-            temp1 = slow.next
-            temp2 = fast.next
-            slow.next = fast
-            fast.next = temp1
-            slow = temp1
-            fast = temp2
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
+        
+        
