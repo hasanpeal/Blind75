c60ead876047ed273ce98477d710bd7db717337e
Peal Hasan <pealh0320@gmail.com>
2025-07-20 13:40:35 -0500

    Daily practice: 2025-07-20 13:40:35

--- practice.py
+++ practice.py
@@ -1,21 +1,38 @@
+from collections import defaultdict
 from typing import List
 
-
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
-        left = [0] * len(nums)
-        right = [0] * len(nums)
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # Step 1: Build the adjacency list to represent the graph
+        # The graph is represented as a dictionary where each node maps to a list of its neighbors
+        adj = defaultdict(list)
+        for a, b in edges:
+            # Add both directions for the undirected graph
+            adj[a].append(b)
+            adj[b].append(a)
+        
+        # Step 2: Initialize a set to keep track of visited nodes
+        visited = set()
 
-        left[0] = 1
-        right[len(nums) - 1] = 1
-
-
-        for i in range(1, len(nums)):
-            left[i] = nums[i-1] * left[i-1]
-        for i in range(len(nums) - 2, -1, -1): # Middle val is -1 which represent 0 !!
-            right[i] = nums[i+1] * right[i+1]
-        for i in range(len(nums)):
-            res[i] = left[i] * right[i]
-
-        return res
+        # Step 3: Define a DFS function to traverse the graph
+        def dfs(node):
+            # Mark the current node as visited
+            visited.add(node)
+            # Traverse all unvisited neighbors of the current node
+            for nei in adj[node]:
+                if nei not in visited:
+                    dfs(nei)
+        
+        # Step 4: Initialize a counter for the number of connected components
+        count = 0
+        
+        # Step 5: Iterate through all nodes
+        for i in range(n):
+            # If a node is not visited, it's part of a new connected component
+            if i not in visited:
+                count += 1  # Increment the count for a new component
+                # Perform DFS to mark all nodes in this component as visited
+                dfs(i)
+        
+        # Step 6: Return the total number of connected components
+        return count
