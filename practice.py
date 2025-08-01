19eea0dc236b4b17f02e51e4ae2f9df4c788ddde
Peal Hasan <pealh0320@gmail.com>
2025-07-31 20:42:50 -0500

    Daily practice: 2025-07-31 20:42:49

--- practice.py
+++ practice.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -7,20 +5,14 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = float("-inf")
-        def postorder(root):
-            # Use nonlocal to access global variable res
-            nonlocal res
-            if not root:
-                return 0
-            # Max in left sub tree, if negative then 0
-            leftMax = max(postorder(root.left), 0)
-            # Max in right sub tree, if negative then 0
-            rightMax = max(postorder(root.right), 0)
-            # Adjacent node check if max then update global res
-            res = max(res, root.val + leftMax + rightMax)
-            # Pass single path max sum for each recursive call
-            return root.val + max(leftMax, rightMax)
-        postorder(root)
-        return res
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        curr = root
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                return curr
