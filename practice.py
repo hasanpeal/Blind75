827071e5facd1dc675539a61d146961ab70a1c92
Peal Hasan <pealh0320@gmail.com>
2025-08-01 18:29:35 -0500

    Daily practice: 2025-08-01 18:29:35

--- practice.py
+++ practice.py
@@ -1,18 +1,23 @@
 from typing import Optional
 
-class ListNode:
-    def __init__(self, val=0, next=None):
+class Node:
+    def __init__(self, val = 0, neighbors = None):
         self.val = val
-        self.next = next
+        self.neighbors = neighbors if neighbors is not None else []
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head.next
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+            
+        oldToNew = {}
 
-        while fast and fast.next:
-            if slow == fast:
-                return True
-            slow = slow.next
-            fast = fast.next.next
-        return False
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+        return dfs(node)
