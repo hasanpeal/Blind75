e9ca7118f83d710085454f216272348b96288e86
Peal Hasan <pealh0320@gmail.com>
2025-08-21 22:13:53 -0400

    Daily practice: 2025-08-21 22:13:53

--- practice.py
+++ practice.py
@@ -1,18 +1,30 @@
 from typing import Optional
+from LinkedListCycle import ListNode
 
-class ListNode:
-    def __init__(self, val=0, next=None):
+def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head.next
-
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
         while fast and fast.next:
-            if slow == fast:
-                return True
             slow = slow.next
             fast = fast.next.next
-        return False
+        curr = slow.next
+        slow.next = None
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow = head
+        fast = prev
+        while fast:
+            temp1 = slow.next
+            temp2 = fast.next
+            slow.next = fast
+            fast.next = temp1
+            slow = temp1
+            fast = temp2
