e2fe06ee4c3146fde4cc0081cc3c583474b84365
Peal Hasan <pealh0320@gmail.com>
2025-09-01 08:42:51 -0400

    Daily practice: 2025-09-01 08:42:51

--- practice.py
+++ practice.py
@@ -1,23 +1,28 @@
-from typing import List
+import heapq
 
-def numIslands(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        count = 0
+class MedianFinder:
 
-        def dfs(r,c):
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
-                return
-            grid[r][c] = "0"
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1":
-                    count += 1
-                    dfs(r,c)
-                    
-        return count
+    def __init__(self):
+        # Store approximate equal values in 2 heap for accessing max and min in O(1)
+        self.maxHeap, self.minHeap =[], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.maxHeap, -1 * num)
+        # Make sure values in heap are there then if highest val of maxHeap greater than minHeaps min then swap
+        if (self.maxHeap and self.minHeap) and -1 * self.maxHeap[0] > self.minHeap[0]:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+        # The length of both heaps difference can't be greater than 1 if it's then swap to equalize
+        if len(self.maxHeap) > len(self.minHeap) + 1:
+            val = -1 * heapq.heappop(self.maxHeap)
+            heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            val = 1 * heapq.heappop(self.minHeap)
+            heapq.heappush(self.maxHeap, -1 * val)
+
+    def findMedian(self) -> float:
+        if len(self.maxHeap) > len(self.minHeap):
+            return -1 * self.maxHeap[0]
+        if len(self.minHeap) > len(self.maxHeap):
+            return self.minHeap[0]
+        return ((-1 * self.maxHeap[0]) + self.minHeap[0])/2
