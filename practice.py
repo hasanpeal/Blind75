aa8f45e2a1553b5932cfc72c6094b3ca090c8c02
Peal Hasan <pealh0320@gmail.com>
2025-09-04 10:57:04 -0400

    Daily practice: 2025-09-04 10:57:04

--- practice.py
+++ practice.py
@@ -1,39 +1,12 @@
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Codec:
-    
-    # Encodes a tree to a single string.
-    def serialize(self, root: Optional[TreeNode]) -> str:
-        res = []
-        def preorder(node):
-            if not node:
-                res.append("N")
-                return
-            res.append(str(node.val))
-            preorder(node.left)
-            preorder(node.right)
-        preorder(root)
-        return ",".join(res)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
         
-    # Decodes your encoded data to tree.
-    def deserialize(self, data: str) -> Optional[TreeNode]:
-        nodes = data.split(",")
-        index = 0
-        def preorder():
-            nonlocal index
-            if nodes[index] == "N":
-                index += 1
-                return None
-            node = TreeNode(int(nodes[index]))
-            index += 1
-            node.left = preorder()
-            node.right = preorder()
-            return node
-        return preorder()
-            
+        
