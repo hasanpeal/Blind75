8ac1ab0a94ac05866d10ab85f052e5d64e32e74f
Peal Hasan <pealh0320@gmail.com>
2025-09-21 12:20:58 -0400

    Daily practice: 2025-09-21 12:20:58

--- practice.py
+++ practice.py
@@ -1,13 +1,13 @@
 from typing import List
 
-def maxProfit(self, prices: List[int]) -> int:
-        pointer1, pointer2, res = 0, 1, 0
-        while(pointer2 < len(prices)):
-            profit = prices[pointer2] - prices[pointer1]
-            res = max(res, profit)
-            if(pointer2 == len(prices) - 1):
-                pointer1 += 1
-                pointer2 = pointer1 + 1
-            else:
-                pointer2 += 1
-        return res
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        has = {}
+        for i in range(len(nums)):
+            has[nums[i]] = i
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in has and has.get(diff) != i:
+                return [i, has.get(diff)]
+        return []
