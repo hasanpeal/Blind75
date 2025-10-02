ea8c768326470f5a03c9568d907ae7d82b21b9f2
Peal Hasan <pealh0320@gmail.com>
2025-10-01 20:39:04 -0400

    Daily practice: 2025-10-01 20:39:04

--- practice.py
+++ practice.py
@@ -1,25 +1,11 @@
 from typing import List
 
-def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preq = {i : [] for i in range(numCourses)}
-        for c, p in prerequisites:
-            preq[c].append(p)
-        visited = set()
-
-        def dfsCycleDetect(c):
-            if c in visited:
-                return False
-            if preq[c] == []:
-                return True
-            visited.add(c)
-            for pre in preq[c]:
-                if not dfsCycleDetect(pre):
-                    return False
-            visited.remove(c)
-            preq[c] = []
-            return True
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        has = {}
+        for i, v in enumerate(numbers):
+            has[v] = i
+        for i in range(len(numbers)):
+            key = target - numbers[i]
+            if key in has and has.get(key) != i:
+                return [min(i+1, has.get(key) + 1), max(i+1, has.get(key) + 1)]
         
-        for p in range(numCourses):
-            if not dfsCycleDetect(p):
-                return False
-        return True
