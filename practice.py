171ed8925c54b5a74d27d34a54f1a69de66c6781
Peal Hasan <pealh0320@gmail.com>
2025-10-05 14:38:52 -0400

    Daily practice: 2025-10-05 14:38:52

--- practice.py
+++ practice.py
@@ -1,14 +1,13 @@
 from typing import List
 
-def maxArea(self, heights: List[int]) -> int:
-        left, right = 0, len(heights) - 1
-        maximum = 0
-
-        while (left < right):
-            currMax = (right - left) * min(heights[left], heights[right])
-            maximum = max(currMax, maximum)
-            if(heights[left] <= heights[right]):
-                left += 1
+def maxProfit(self, prices: List[int]) -> int:
+        pointer1, pointer2, res = 0, 1, 0
+        while(pointer2 < len(prices)):
+            profit = prices[pointer2] - prices[pointer1]
+            res = max(res, profit)
+            if(pointer2 == len(prices) - 1):
+                pointer1 += 1
+                pointer2 = pointer1 + 1
             else:
-                right -= 1
-        return maximum
+                pointer2 += 1
+        return res
