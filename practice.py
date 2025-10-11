89f4359b0573d9df355c30d0253d3a27630bf03c
Peal Hasan <pealh0320@gmail.com>
2025-10-10 22:34:43 -0400

    Daily practice: 2025-10-10 22:34:43

--- practice.py
+++ practice.py
@@ -1,23 +1,14 @@
 from typing import Optional
-
-class Node:
-    def __init__(self, val = 0, neighbors = None):
+class ListNode:
+    def __init__(self, val=0, next=None):
         self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
+        self.next = next
 
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
-            
-        oldToNew = {}
-
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
-        return dfs(node)
+def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
