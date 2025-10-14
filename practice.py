66328d70c15085e64b91a3efd766bee536deaaf9
Peal Hasan <pealh0320@gmail.com>
2025-10-14 18:01:15 -0400

    Daily practice: 2025-10-14 18:01:15

--- practice.py
+++ practice.py
@@ -1,12 +1,25 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        mapS = {}
-        mapT = {}
-        for i in range(len(s)):
-            mapS[s[i]] = 1 + mapS.get(s[i], 0)
-            mapT[t[i]] = 1 + mapT.get(t[i], 0)
-        return mapS == mapT
+from typing import List
+
+def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preq = {i : [] for i in range(numCourses)}
+        for c, p in prerequisites:
+            preq[c].append(p)
+        visited = set()
+
+        def dfsCycleDetect(c):
+            if c in visited:
+                return False
+            if preq[c] == []:
+                return True
+            visited.add(c)
+            for pre in preq[c]:
+                if not dfsCycleDetect(pre):
+                    return False
+            visited.remove(c)
+            preq[c] = []
+            return True
         
-        
+        for p in range(numCourses):
+            if not dfsCycleDetect(p):
+                return False
+        return True
