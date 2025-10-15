db5ef14491d3aabb98bde10ea5b137c722a1e55d
Peal Hasan <pealh0320@gmail.com>
2025-10-15 12:35:56 -0400

    Daily practice: 2025-10-15 12:35:56

--- practice.py
+++ practice.py
@@ -1,28 +1,12 @@
-from typing import List, Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution:    
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
-            return None
-        for i in range(1, len(lists)):
-            lists[i] = self.merge(lists[i-1], lists[i])
-        return lists[-1]
-
-    def merge(self, list1, list2):
-        dummy = ListNode()
-        curr = dummy
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next
-        curr.next = list1 or list2
-        return dummy.next
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
+        
+        
