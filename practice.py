5425efc9007a992f7f9cbf56ca8fab7bd274c564
Peal Hasan <pealh0320@gmail.com>
2025-10-27 14:53:55 -0400

    Daily practice: 2025-10-27 14:53:55

--- practice.py
+++ practice.py
@@ -1,13 +1,2 @@
-from typing import List
-
-def maxProfit(self, prices: List[int]) -> int:
-        pointer1, pointer2, res = 0, 1, 0
-        while(pointer2 < len(prices)):
-            profit = prices[pointer2] - prices[pointer1]
-            res = max(res, profit)
-            if(pointer2 == len(prices) - 1):
-                pointer1 += 1
-                pointer2 = pointer1 + 1
-            else:
-                pointer2 += 1
-        return res
+gitscript.py
+script.py
