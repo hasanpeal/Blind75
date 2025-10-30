d983f0de5e73b1f263e9e1c9ebb2bca4c1d36195
Peal Hasan <pealh0320@gmail.com>
2025-10-29 21:00:08 -0400

    Daily practice: 2025-10-29 21:00:08

--- practice.py
+++ practice.py
@@ -1,61 +1,39 @@
-from typing import List
+from typing import Optional
 
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        """
-        Return all unique combinations of 'candidates' that sum up to 'target'.
-        Each candidate may be chosen multiple times.
-        
-        We'll use a DP array 'dp' of length (target + 1).
-        dp[i] will be a list of *all* combinations that sum up to i.
-        """
-        
-        # Step 1: Create the DP array.
-        # Initially, dp = [ [], [], [], ..., [] ] (target+1 empty lists).
-        dp = [[] for _ in range(target + 1)]
-        
-        # VISUAL (initial state):
-        #
-        #    Index:   0     1     2     3     4     5     6     7
-        #    dp:    [  ]   [  ]   [  ]   [  ]   [  ]   [  ]   [  ]   [  ]
-        #
-        # dp[0], dp[1], dp[2], ..., dp[7] are all empty lists right now.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-        # Step 2: Process each candidate in the list.
-        for c in candidates:
-            # We'll try to form sums from 1 up to 'target' using candidate c.
-            for i in range(1, target + 1):
-                
-                # If i is smaller than c, c can't be used to form i,
-                # so we skip to the next i.
-                if i < c:
-                    continue
-                
-                # If i exactly equals c, that means a single combo [c] forms i.
-                if i == c:
-                    dp[i].append([c])
-                    #
-                    # VISUAL: for i == c,
-                    #   dp[i] += [ [c] ]
-                    # Example: if c=2 and i=2, dp[2] = [ [2] ].
-                    #
-                    # Over time, you might see dp (for the example c=2, i=2):
-                    #   dp[2] = [[2]]
-                    
-                else:
-                    # i > c, so we check dp[i - c].
-                    # dp[i - c] holds all combos that sum to i-c.
-                    # By appending c to each of those combos,
-                    # we get new combos that sum to i.
-                    for combo in dp[i - c]:
-                        dp[i].append(combo + [c])
-                        #
-                        # VISUAL:
-                        #   dp[i] += [ combo + [c] for each combo in dp[i - c] ]
-                        #
-                        # Example: if i=5, c=2, then we look at dp[3].
-                        # if dp[3] = [[3]], then we form [[3] + [2]] = [[3, 2]] 
-                        # and append that to dp[5].
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def preorder(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            preorder(node.left)
+            preorder(node.right)
+        preorder(root)
+        return ",".join(res)
         
-        # Step 3: At the end, dp[target] has all unique combos summing to 'target'.
-        return dp[target]
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        nodes = data.split(",")
+        index = 0
+        def preorder():
+            nonlocal index
+            if nodes[index] == "N":
+                index += 1
+                return None
+            node = TreeNode(int(nodes[index]))
+            index += 1
+            node.left = preorder()
+            node.right = preorder()
+            return node
+        return preorder()
+            
