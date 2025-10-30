f89bdaf1bce09dd41d9f44a8c48374b83e52a131
Peal Hasan <pealh0320@gmail.com>
2025-10-29 21:00:02 -0400

    Daily practice: 2025-10-29 21:00:02

--- practice.py
+++ practice.py
@@ -1,11 +1,12 @@
-from typing import List
-
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        has = {}
-        for i, v in enumerate(numbers):
-            has[v] = i
-        for i in range(len(numbers)):
-            key = target - numbers[i]
-            if key in has and has.get(key) != i:
-                return [min(i+1, has.get(key) + 1), max(i+1, has.get(key) + 1)]
-        
+def lengthOfLongestSubstring(s: str) -> int:
+        seen = set()
+        left, right, longest = 0, 0, 0
+        while(right < len(s)):
+            if s[right] not in seen:
+                seen.add(s[right])
+                longest = max(longest, len(seen))
+                right += 1
+            else:
+                seen.remove(s[left])
+                left += 1
+        return longest
