3fa768b7b4d19399920da5ea071509c3eab49534
Peal Hasan <pealh0320@gmail.com>
2025-11-05 19:21:30 -0500

    Daily practice: 2025-11-05 19:21:30

--- practice.py
+++ practice.py
@@ -1,20 +1,30 @@
-from typing import Optional
+from typing import List
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        left = dummy
-        right = head
-        while n > 0 and right:
-            right = right.next
-            n -= 1
-        while right:
-            left = left.next
-            right = right.next
-        left.next = left.next.next
-        return dummy.next
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        # Numerical length + '#' + string itself
+        for s in strs:
+            res += str(len(s)) + '#' + s
+        return res
+
+    # Ex. 3#abc4#abcd
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        # Outer loop runs from 0 to n, inside each iteration we use another var and set it equal to current index
+        # Increment j until we see the character '#', then we can slice s[i:j] to get the length of the string then
+        # reassign i to index of '#' + 1 which is start of the string, and set j to i + length.
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+        return res
+    print(decode(s='3#abc4#abcd'))
