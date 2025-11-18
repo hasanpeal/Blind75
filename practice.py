10462d92b9422329d0bcf12103e651a0d97e1058
Peal Hasan <pealh0320@gmail.com>
2025-11-17 21:06:23 -0500

    Daily practice: 2025-11-17 21:06:23

--- practice.py
+++ practice.py
@@ -1,14 +1,13 @@
-from typing import Optional
+from typing import List
 
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return max((1 + self.maxDepth(root.left)), ((1 + self.maxDepth(root.right))))
-        
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+                
+        return False
