66aff6656862f8f6c3dfbd9751ac1b692cde6224
Peal Hasan <pealh0320@gmail.com>
2025-12-10 20:29:54 -0500

    Daily practice: 2025-12-10 20:29:54

--- practice.py
+++ practice.py
@@ -1,16 +1,14 @@
 from typing import List
 
-def findMin(self, nums: List[int]) -> int:
-        l, r, res = 0, len(nums) - 1, nums[0]
-        # It's '<=' because what if we have [2] here l val = 2 r val = 2, it would skip loop for '<'
-        while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
-                break
-            m = (l + r) // 2
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
+def maxArea(self, heights: List[int]) -> int:
+        left, right = 0, len(heights) - 1
+        maximum = 0
+
+        while (left < right):
+            currMax = (right - left) * min(heights[left], heights[right])
+            maximum = max(currMax, maximum)
+            if(heights[left] <= heights[right]):
+                left += 1
             else:
-                r = m - 1
-        return res
+                right -= 1
+        return maximum
