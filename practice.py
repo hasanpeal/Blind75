d634a6bab82ff825136dd1e81b922ee6c4e56b4e
Peal Hasan <pealh0320@gmail.com>
2025-12-19 15:00:32 -0500

    Daily practice: 2025-12-19 15:00:32

--- practice.py
+++ practice.py
@@ -7,12 +7,8 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return None
-        temp = root.left
-        root.left = root.right
-        root.right = temp
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-        return root
+            return 0
+        return max((1 + self.maxDepth(root.left)), ((1 + self.maxDepth(root.right))))
+        
