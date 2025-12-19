c399fda432c69e3997e2516c16f3bab0e0db92e7
Peal Hasan <pealh0320@gmail.com>
2025-12-19 14:59:50 -0500

    Daily practice: 2025-12-19 14:59:50

--- practice.py
+++ practice.py
@@ -1,2 +1,21 @@
-gitscript.py
-script.py
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        left = [0] * len(nums)
+        right = [0] * len(nums)
+
+        left[0] = 1
+        right[len(nums) - 1] = 1
+
+
+        for i in range(1, len(nums)):
+            left[i] = nums[i-1] * left[i-1]
+        for i in range(len(nums) - 2, -1, -1): # Middle val is -1 which represent 0 !!
+            right[i] = nums[i+1] * right[i+1]
+        for i in range(len(nums)):
+            res[i] = left[i] * right[i]
+
+        return res
