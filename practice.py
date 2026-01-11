43711a3a52afc5e40d46b6cd47bd966d571df2fc
Peal Hasan <pealh0320@gmail.com>
2026-01-11 12:56:48 -0500

    Daily practice: 2026-01-11 12:56:48

--- practice.py
+++ practice.py
@@ -7,8 +7,14 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return max((1 + self.maxDepth(root.left)), ((1 + self.maxDepth(root.right))))
-        
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        nodes = []
+        # Inorder traversal sorted the BST
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            nodes.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return nodes[k - 1]
