b3554f8db72fc417550ca395245af6d7f751b0f2
Peal Hasan <pealh0320@gmail.com>
2026-01-16 11:33:39 -0500

    Daily practice: 2026-01-16 11:33:39

--- practice.py
+++ practice.py
@@ -1,61 +1,31 @@
 from typing import List
 
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        """
-        Return all unique combinations of 'candidates' that sum up to 'target'.
-        Each candidate may be chosen multiple times.
-        
-        We'll use a DP array 'dp' of length (target + 1).
-        dp[i] will be a list of *all* combinations that sum up to i.
-        """
-        
-        # Step 1: Create the DP array.
-        # Initially, dp = [ [], [], [], ..., [] ] (target+1 empty lists).
-        dp = [[] for _ in range(target + 1)]
-        
-        # VISUAL (initial state):
-        #
-        #    Index:   0     1     2     3     4     5     6     7
-        #    dp:    [  ]   [  ]   [  ]   [  ]   [  ]   [  ]   [  ]   [  ]
-        #
-        # dp[0], dp[1], dp[2], ..., dp[7] are all empty lists right now.
+def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows = len(heights)
+        cols = len(heights[0])
+        pac, atl = set(), set()
+        res = []
 
-        # Step 2: Process each candidate in the list.
-        for c in candidates:
-            # We'll try to form sums from 1 up to 'target' using candidate c.
-            for i in range(1, target + 1):
-                
-                # If i is smaller than c, c can't be used to form i,
-                # so we skip to the next i.
-                if i < c:
-                    continue
-                
-                # If i exactly equals c, that means a single combo [c] forms i.
-                if i == c:
-                    dp[i].append([c])
-                    #
-                    # VISUAL: for i == c,
-                    #   dp[i] += [ [c] ]
-                    # Example: if c=2 and i=2, dp[2] = [ [2] ].
-                    #
-                    # Over time, you might see dp (for the example c=2, i=2):
-                    #   dp[2] = [[2]]
-                    
-                else:
-                    # i > c, so we check dp[i - c].
-                    # dp[i - c] holds all combos that sum to i-c.
-                    # By appending c to each of those combos,
-                    # we get new combos that sum to i.
-                    for combo in dp[i - c]:
-                        dp[i].append(combo + [c])
-                        #
-                        # VISUAL:
-                        #   dp[i] += [ combo + [c] for each combo in dp[i - c] ]
-                        #
-                        # Example: if i=5, c=2, then we look at dp[3].
-                        # if dp[3] = [[3]], then we form [[3] + [2]] = [[3, 2]] 
-                        # and append that to dp[5].
+        def dfs(r, c, visited, minFlow):
+            if r < 0 or c < 0 or r >= rows or c >= cols or heights[r][c] < minFlow or (r,c) in visited:
+                return
+            visited.add((r,c))
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
         
-        # Step 3: At the end, dp[target] has all unique combos summing to 'target'.
-        return dp[target]
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows - 1][c])
+        
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols-1])
+        
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    res.append([r,c])
+        
+        return res
