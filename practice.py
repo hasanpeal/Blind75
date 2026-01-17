9210a4f45efbe2ea2063eaa04ed2024e28a404dd
Peal Hasan <pealh0320@gmail.com>
2026-01-17 11:24:23 -0500

    Daily practice: 2026-01-17 11:24:23

--- practice.py
+++ practice.py
@@ -1,13 +1,27 @@
-from typing import List
+import collections
+from typing import List, Optional
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            else:
-                seen.add(num)
-                
-        return False
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            length = len(q)
+            nodes = []
+            for i in range(length):
+                node = q.popleft()
+                if node:
+                    nodes.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if nodes:
+                res.append(nodes)
+        return res
