1c87961a31e5bd3df852ad3855f6d429f5355483
Peal Hasan <pealh0320@gmail.com>
2026-01-21 18:21:22 -0500

    Daily practice: 2026-01-21 18:21:22

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -1,14 +1,20 @@
 from typing import List
-
-def maxArea(self, heights: List[int]) -> int:
-        left, right = 0, len(heights) - 1
-        maximum = 0
-
-        while (left < right):
-            currMax = (right - left) * min(heights[left], heights[right])
-            maximum = max(currMax, maximum)
-            if(heights[left] <= heights[right]):
-                left += 1
-            else:
+class Solution:
+    def max_area(self, heights: List[int]):
+        if not heights:
+            return 0
+        maxArea = 0
+        left = 0
+        right = len(heights) - 1
+        while left < right:
+            height = min(heights[left], heights[right])
+            width = right - left
+            maxArea = max(maxArea, height*width)
+            if heights[right] < heights[left]:
                 right -= 1
-        return maximum
+            else:
+                left += 1
+        return maxArea
+    
+    heights = [3, 4, 1, 2, 2, 4, 1, 3, 2]
+    print("Max area:", max_area(max_area, heights))
