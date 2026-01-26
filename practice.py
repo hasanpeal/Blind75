a747555c5dde724a6a238b118005aaad2137d190
Peal Hasan <pealh0320@gmail.com>
2026-01-26 16:56:54 -0500

    Daily practice: 2026-01-26 16:56:54

--- practice.py
+++ practice.py
@@ -1,13 +1,30 @@
-from typing import List
+from typing import Optional
+from LinkedListCycle import ListNode
 
+def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        has = {}
-        for i in range(len(nums)):
-            has[nums[i]] = i
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in has and has.get(diff) != i:
-                return [i, has.get(diff)]
-        return []
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        curr = slow.next
+        slow.next = None
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow = head
+        fast = prev
+        while fast:
+            temp1 = slow.next
+            temp2 = fast.next
+            slow.next = fast
+            fast.next = temp1
+            slow = temp1
+            fast = temp2
