786259d0bfd1205c4ff64e67eabd0c816362ce44
Peal Hasan <pealh0320@gmail.com>
2026-02-10 13:41:36 -0500

    Daily practice: 2026-02-10 13:41:36

--- practice.py
+++ practice.py
@@ -1,14 +1,30 @@
 from typing import Optional
-class ListNode:
-    def __init__(self, val=0, next=None):
+from LinkedListCycle import ListNode
+
+def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        curr = slow.next
+        slow.next = None
+        prev = None
         while curr:
             temp = curr.next
             curr.next = prev
             prev = curr
             curr = temp
-        return prev
+        slow = head
+        fast = prev
+        while fast:
+            temp1 = slow.next
+            temp2 = fast.next
+            slow.next = fast
+            fast.next = temp1
+            slow = temp1
+            fast = temp2
