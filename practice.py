30035c1cd3bd7be507df1b3a125cbb2298f3907f
Peal Hasan <pealh0320@gmail.com>
2026-02-19 10:33:07 -0500

    Daily practice: 2026-02-19 10:33:07

--- practice.py
+++ practice.py
@@ -1,31 +1,12 @@
-from typing import List
-
-def isValidSudoku(board: List[List[str]]) -> bool:
-        seen = set()
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                curr = board[i][j]
-                if curr != ".":
-                    row = f"{curr} in row {i}"
-                    col = f"{curr} in col {j}"
-                    box = f"{curr} in box {(i//3 * 3) + j//3}"
-                    print(box)
-                    if row in seen or col in seen or box in seen:
-                        return False
-                    else:
-                        seen.add(row)
-                        seen.add(col)
-                        seen.add(box)
-        return True
-
-board = [["1","2",".",".","3",".",".",".","."],
-        ["4",".",".","5",".",".",".",".","."],
-        [".","9","8",".",".",".",".",".","3"],
-        ["5",".",".",".","6",".",".",".","4"],
-        [".",".",".","8",".","3",".",".","5"],
-        ["7",".",".",".","2",".",".",".","6"],
-        [".",".",".",".",".",".","2",".","."],
-        [".",".",".","4","1","9",".",".","8"],
-        [".",".",".",".","8",".",".","7","9"]]
-
-isValidSudoku(board)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS = {}
+        mapT = {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
+        
+        
