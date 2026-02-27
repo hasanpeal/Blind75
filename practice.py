540137b74ccb52855577da28a15f7ac16d0dea92
Peal Hasan <pealh0320@gmail.com>
2026-02-27 16:41:33 -0500

    Daily practice: 2026-02-27 16:41:33

--- practice.py
+++ practice.py
@@ -6,13 +6,15 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head.next
-
-        while fast and fast.next:
-            if slow == fast:
-                return True
-            slow = slow.next
-            fast = fast.next.next
-        return False
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+        while right:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
+        return dummy.next
