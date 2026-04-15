c3b62cf3fea36467146122b845d4990e79324aa7
Peal Hasan <pealh0320@gmail.com>
2026-04-15 18:57:07 -0400

    Daily practice: 2026-04-15 18:57:07

--- practice.py
+++ practice.py
@@ -1,12 +1,2 @@
-def lengthOfLongestSubstring(s: str) -> int:
-        seen = set()
-        left, right, longest = 0, 0, 0
-        while(right < len(s)):
-            if s[right] not in seen:
-                seen.add(s[right])
-                longest = max(longest, len(seen))
-                right += 1
-            else:
-                seen.remove(s[left])
-                left += 1
-        return longest
+gitscript.py
+script.py
