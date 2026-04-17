6784d9dfa4f1ae0ab7bd4475f0f4e0dfa699af25
Peal Hasan <pealh0320@gmail.com>
2026-04-17 12:50:49 -0400

    Daily practice: 2026-04-17 12:50:48

--- practice.py
+++ practice.py
@@ -1,20 +1,13 @@
-from typing import Optional
+from typing import List
 
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        left = dummy
-        right = head
-        while n > 0 and right:
-            right = right.next
-            n -= 1
-        while right:
-            left = left.next
-            right = right.next
-        left.next = left.next.next
-        return dummy.next
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+                
+        return False
