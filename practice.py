735066e95779d0cb853425e3de16e89a4bda86d2
Peal <pealh0320@gmail.com>
2026-04-29 14:28:24 -0400

    Daily practice: 2026-04-29 14:28:24

--- practice.py
+++ practice.py
@@ -1,19 +1,11 @@
 from typing import List
 
-def threeSum(nums: List[int]) -> List[List[int]]:
-        res = []
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
         has = {}
-        for i, v in enumerate(nums):
+        for i, v in enumerate(numbers):
             has[v] = i
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums), 1):
-                key = -nums[i] - nums[j]
-                if key in has and has.get(key) != i and has.get(key) != j:
-                    triplet = [nums[i], nums[j], key]
-                    triplet.sort()
-                    if triplet not in res:
-                        res.append(triplet)
-                        
-        print(has)
-        return res
-print(threeSum([-1,0,1,2,-1,-4]))
+        for i in range(len(numbers)):
+            key = target - numbers[i]
+            if key in has and has.get(key) != i:
+                return [min(i+1, has.get(key) + 1), max(i+1, has.get(key) + 1)]
+        
