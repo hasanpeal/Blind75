1858506af0f15e7ad0ee76818e1dadcb5e941bc9
Peal <pealh0320@gmail.com>
2026-05-07 19:30:01 -0400

    Daily practice: 2026-05-07 19:30:01

--- practice.py
+++ practice.py
@@ -1,2 +1,13 @@
-gitscript.py
-script.py
+from typing import List
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen.add(num)
+                
+        return False
