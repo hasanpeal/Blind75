4eae1ec4279423b79b5403473ee0cef003b9967a
Peal <pealh0320@gmail.com>
2026-05-10 16:00:00 -0400

    Daily practice: 2026-05-10 16:00:00

--- practice.py
+++ practice.py
@@ -1,13 +1,20 @@
 from typing import List
-
-
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
+    def max_area(self, heights: List[int]):
+        if not heights:
+            return 0
+        maxArea = 0
+        left = 0
+        right = len(heights) - 1
+        while left < right:
+            height = min(heights[left], heights[right])
+            width = right - left
+            maxArea = max(maxArea, height*width)
+            if heights[right] < heights[left]:
+                right -= 1
             else:
-                seen.add(num)
-                
-        return False
+                left += 1
+        return maxArea
+    
+    heights = [3, 4, 1, 2, 2, 4, 1, 3, 2]
+    print("Max area:", max_area(max_area, heights))
