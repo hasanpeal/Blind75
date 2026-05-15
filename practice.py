d1799d2c985373d2c1e5002a59b2a5b1847ed5ba
Peal <pealh0320@gmail.com>
2026-05-15 19:30:00 -0400

    Daily practice: 2026-05-15 19:30:00

--- practice.py
+++ practice.py
@@ -1,16 +1,13 @@
 from typing import List
 
-def findMin(self, nums: List[int]) -> int:
-        l, r, res = 0, len(nums) - 1, nums[0]
-        # It's '<=' because what if we have [2] here l val = 2 r val = 2, it would skip loop for '<'
-        while l <= r:
-            if nums[l] < nums[r]:
-                res = min(res, nums[l])
-                break
-            m = (l + r) // 2
-            res = min(res, nums[m])
-            if nums[m] >= nums[l]:
-                l = m + 1
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
             else:
-                r = m - 1
-        return res
+                seen.add(num)
+                
+        return False
