44b7ae812e9234a53d2b6c0d9b06d75390a8a98e
Peal <pealh0320@gmail.com>
2026-05-16 16:00:00 -0400

    Daily practice: 2026-05-16 16:00:00

--- practice.py
+++ practice.py
@@ -1,13 +1,30 @@
-from typing import List
+from typing import Optional
+from LinkedListCycle import ListNode
 
+def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            else:
-                seen.add(num)
-                
-        return False
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        curr = slow.next
+        slow.next = None
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow = head
+        fast = prev
+        while fast:
+            temp1 = slow.next
+            temp2 = fast.next
+            slow.next = fast
+            fast.next = temp1
+            slow = temp1
+            fast = temp2
