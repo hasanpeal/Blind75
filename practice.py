8cc73b2fa6a7d5acbabd49ee379236263c892d2e
Peal <pealh0320@gmail.com>
2026-05-17 13:30:01 -0400

    Daily practice: 2026-05-17 13:30:01

--- practice.py
+++ practice.py
@@ -2,20 +2,12 @@ from typing import List
 
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
-        left = [0] * len(nums)
-        right = [0] * len(nums)
-
-        left[0] = 1
-        right[len(nums) - 1] = 1
-
-
-        for i in range(1, len(nums)):
-            left[i] = nums[i-1] * left[i-1]
-        for i in range(len(nums) - 2, -1, -1): # Middle val is -1 which represent 0 !!
-            right[i] = nums[i+1] * right[i+1]
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        has = {}
         for i in range(len(nums)):
-            res[i] = left[i] * right[i]
-
-        return res
+            has[nums[i]] = i
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in has and has.get(diff) != i:
+                return [i, has.get(diff)]
+        return []
