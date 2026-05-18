9fa6ade2b49c4df2938ffd503539e4416e68b23b
Peal <pealh0320@gmail.com>
2026-05-18 15:30:01 -0400

    Daily practice: 2026-05-18 15:30:01

--- practice.py
+++ practice.py
@@ -1,20 +1,23 @@
-from typing import List
-class Solution:
-    def max_area(self, heights: List[int]):
-        if not heights:
-            return 0
-        maxArea = 0
-        left = 0
-        right = len(heights) - 1
-        while left < right:
-            height = min(heights[left], heights[right])
-            width = right - left
-            maxArea = max(maxArea, height*width)
-            if heights[right] < heights[left]:
-                right -= 1
-            else:
-                left += 1
-        return maxArea
-    
-    heights = [3, 4, 1, 2, 2, 4, 1, 3, 2]
-    print("Max area:", max_area(max_area, heights))
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot or self.isSameTree(root, subRoot):
+            return True
+        if not root:
+            return False
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+         
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        elif (p and q) and (p.val == q.val):
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
