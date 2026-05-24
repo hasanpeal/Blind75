a34e49efda18d6f1583741d31196712092db5095
Peal <pealh0320@gmail.com>
2026-05-24 14:00:01 -0400

    Daily practice: 2026-05-24 14:00:01

--- practice.py
+++ practice.py
@@ -1,13 +1,25 @@
 from typing import List
 
+def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preq = {i : [] for i in range(numCourses)}
+        for c, p in prerequisites:
+            preq[c].append(p)
+        visited = set()
 
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
+        def dfsCycleDetect(c):
+            if c in visited:
+                return False
+            if preq[c] == []:
                 return True
-            else:
-                seen.add(num)
-                
-        return False
+            visited.add(c)
+            for pre in preq[c]:
+                if not dfsCycleDetect(pre):
+                    return False
+            visited.remove(c)
+            preq[c] = []
+            return True
+        
+        for p in range(numCourses):
+            if not dfsCycleDetect(p):
+                return False
+        return True
