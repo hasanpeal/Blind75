46f3a3f0ae5259febbe0a11cb3accb9702b2c9fa
Peal <pealh0320@gmail.com>
2026-05-29 21:00:01 -0400

    Daily practice: 2026-05-29 21:00:01

--- practice.py
+++ practice.py
@@ -1,27 +1,2 @@
-import collections
-from typing import List, Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        q = collections.deque()
-        q.append(root)
-
-        while q:
-            length = len(q)
-            nodes = []
-            for i in range(length):
-                node = q.popleft()
-                if node:
-                    nodes.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if nodes:
-                res.append(nodes)
-        return res
+gitscript.py
+script.py
