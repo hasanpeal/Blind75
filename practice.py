ddd364da41c727f0931b6fb8f4ed49f49d564ad3
Peal <pealh0320@gmail.com>
2026-05-29 20:00:00 -0400

    Daily practice: 2026-05-29 20:00:00

--- practice.py
+++ practice.py
@@ -1,6 +1,3 @@
-import collections
-from typing import List, Optional
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -8,20 +5,14 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        q = collections.deque()
-        q.append(root)
-
-        while q:
-            length = len(q)
-            nodes = []
-            for i in range(length):
-                node = q.popleft()
-                if node:
-                    nodes.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-            if nodes:
-                res.append(nodes)
-        return res
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        curr = root
+        while curr:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                return curr
