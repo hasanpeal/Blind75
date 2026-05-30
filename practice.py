cb33b2a97a4dc45b29798f16369ad63b59735f52
Peal <pealh0320@gmail.com>
2026-05-30 10:00:01 -0400

    Daily practice: 2026-05-30 10:00:01

--- practice.py
+++ practice.py
@@ -1,40 +1,16 @@
-from collections import defaultdict
 from typing import List
 
-class Solution:
-    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # Step 1: Build the adjacency list to represent the graph
-        # The graph is represented as a dictionary where each node maps to a list of its neighbors
-        adj = defaultdict(list)
-        for a, b in edges:
-            # Add both directions for the undirected graph
-            adj[a].append(b)
-            adj[b].append(a)
-        
-        # Step 2: Initialize a set to keep track of visited nodes
-        visited = set()
-
-        # Step 3: Define a DFS function to traverse the graph
-        def dfs(node):
-            # Mark the current node as visited
-            visited.add(node)
-            # Traverse all unvisited neighbors of the current node
-            for nei in adj[node]:
-                if nei not in visited:
-                    dfs(nei)
-        
-        # Step 4: Initialize a counter for the number of connected components
-        count = 0
-        
-        # Step 5: Iterate through all nodes
-        for i in range(n):
-            # If a node is not visited, it's part of a new connected component
-            if i not in visited:
-                count += 1  # Increment the count for a new component
-                # Perform DFS to mark all nodes in this component as visited
-                dfs(i)
-                
-                
-        
-        # Step 6: Return the total number of connected components
-        return count
+def findMin(self, nums: List[int]) -> int:
+        l, r, res = 0, len(nums) - 1, nums[0]
+        # It's '<=' because what if we have [2] here l val = 2 r val = 2, it would skip loop for '<'
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        return res
