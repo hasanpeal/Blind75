3cb7f4c3baa0cfe6f67cb1cc97897c91ed4dd9c4
Peal <pealh0320@gmail.com>
2026-06-13 17:17:41 -0400

    Daily practice: 2026-06-13 17:17:41

--- practice.py
+++ practice.py
@@ -1,21 +1,20 @@
 from typing import List
 
-
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [0] * len(nums)
-        left = [0] * len(nums)
-        right = [0] * len(nums)
-
-        left[0] = 1
-        right[len(nums) - 1] = 1
-
-
-        for i in range(1, len(nums)):
-            left[i] = nums[i-1] * left[i-1]
-        for i in range(len(nums) - 2, -1, -1): # Middle val is -1 which represent 0 !!
-            right[i] = nums[i+1] * right[i+1]
+def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        res = 1
+        has = set()
+        for num in nums:
+            has.add(num)
         for i in range(len(nums)):
-            res[i] = left[i] * right[i]
-
+            curr = nums[i]
+            longest = 1
+            if curr - 1 in has:
+                continue
+            while curr + 1 in has:
+                curr += 1
+                longest += 1
+            res = max(res, longest)
         return res
+            
