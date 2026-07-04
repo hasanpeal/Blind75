01463febc55d6b4f58b9b7783f6eff4e3dcef16b
Peal <pealh0320@gmail.com>
2026-07-04 11:37:31 -0400

    Daily practice: 2026-07-04 11:37:31

--- practice.py
+++ practice.py
@@ -1,13 +1,12 @@
-from typing import List
-
-def maxProfit(self, prices: List[int]) -> int:
-        pointer1, pointer2, res = 0, 1, 0
-        while(pointer2 < len(prices)):
-            profit = prices[pointer2] - prices[pointer1]
-            res = max(res, profit)
-            if(pointer2 == len(prices) - 1):
-                pointer1 += 1
-                pointer2 = pointer1 + 1
+def lengthOfLongestSubstring(s: str) -> int:
+        seen = set()
+        left, right, longest = 0, 0, 0
+        while(right < len(s)):
+            if s[right] not in seen:
+                seen.add(s[right])
+                longest = max(longest, len(seen))
+                right += 1
             else:
-                pointer2 += 1
-        return res
+                seen.remove(s[left])
+                left += 1
+        return longest
