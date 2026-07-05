41110ef195cbf6e7cf7ea97a23f7b1ac4785c612
Peal <pealh0320@gmail.com>
2026-07-05 13:33:28 -0400

    Daily practice: 2026-07-05 13:33:28

--- practice.py
+++ practice.py
@@ -1,13 +1,11 @@
 from typing import List
 
-
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            else:
-                seen.add(num)
-                
-        return False
+def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        has = {}
+        for i, v in enumerate(numbers):
+            has[v] = i
+        for i in range(len(numbers)):
+            key = target - numbers[i]
+            if key in has and has.get(key) != i:
+                return [min(i+1, has.get(key) + 1), max(i+1, has.get(key) + 1)]
+        
